97471fd0a56895dbb5b20642cb32baa08a68879e
t0x1c <dextorantony@yahoo.com>
2019-07-09 10:33:35 +0530

    1.Migrated to AndroidX 2.Type cast & Code cleanup 3.Added SignUp Button

--- FirebaseSaveUserData/app/src/main/java/com/tabian/firebasesaveuserdata/AddToDatabase.java
+++ FirebaseSaveUserData/app/src/main/java/com/tabian/firebasesaveuserdata/AddToDatabase.java
@@ -1,9 +1,9 @@
 package com.tabian.firebasesaveuserdata;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -40,10 +40,10 @@ public class AddToDatabase extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_to_database_layout);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        mName = (EditText) findViewById(R.id.etName);
-        mEmail = (EditText) findViewById(R.id.etEmail);
-        mPhoneNum = (EditText) findViewById(R.id.etPhone);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        mName = findViewById(R.id.etName);
+        mEmail = findViewById(R.id.etEmail);
+        mPhoneNum = findViewById(R.id.etPhone);
 
         //declare the database reference object. This is what we use to access the database.
         //NOTE: Unless you are signed in, this will not be useable.
